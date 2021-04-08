3a4bbd02279723610193eac342c693ee863722a0
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2021-04-08 02:38:58 -0400

    Add Move Towards integer extension

---
+++ MathExtensions.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mox
+{
+    public static partial class Extensions
+    {
+        public static int MoveTowards(this int value, int toward, int amount)
+		{
+            if (Mathf.Abs(toward - value) <= amount)
+                return toward;
+
+            if (value > toward)
+                return value - amount;
+
+            return value + amount;
+		}
+    }
+}
