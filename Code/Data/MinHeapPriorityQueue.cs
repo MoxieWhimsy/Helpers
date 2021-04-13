9c7e6191e87d0509a853c152f67486e3eb63c7d1
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2021-04-13 00:20:27 -0400

    Add MinHeapify

---
+++ Code/Data/MinHeapPriorityQueue.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Mox.Data
+{
+    public class MinHeapPriorityQueue<T> : IPriorityQueue<T>
+    {
+        readonly List<HeapNode> nodes = new List<HeapNode>();
+
+        public int Count => nodes.Count;
+
+        public bool Dequeue(out T result)
+        {
+            if (nodes.Count <= 0)
+            {
+                result = default;
+                return false;
+            }
+            result = nodes[0].value;
+            nodes[0] = nodes[Count - 1];
+            nodes.RemoveAt(Count - 1);
+            nodes.MinHeapify(Count, 0);
+            return true;
+        }
+
+        public void Enqueue(T item, int priority)
+        {
+            nodes.Insert(0, new HeapNode(item, priority));
+            nodes.MinHeapify(Count, 0);
+        }
+
+        class HeapNode : IHasPriority
+        {
+            public T value;
+            readonly int _priority;
+
+            public HeapNode(T value, int priority)
+            {
+                this.value = value;
+                _priority = priority;
+            }
+            public int Priority
+            {
+                get => _priority;
+            }
+
+            public override string ToString() => $"'{value}' p{_priority}";
+        }
+
+        public override string ToString() => string.Join(", ", nodes);
+    }
+}
--- Code/DataStructureExtensions.cs
+++ Code/DataStructureExtensions.cs
@@ -22,6 +22,24 @@ namespace Mox
 				list.Heapify(last, largest);
 			}
 		}
+		public static void MinHeapify<T>(this List<T> list, int last, int index) where T : IHasPriority
+		{
+			int smallest = index;
+			int left = 2 * index + 1;
+			int right = 2 * index + 2;
+
+			if (left < last && list[index].Priority > list[left].Priority)
+				smallest = left;
+			if (right < last && list[smallest].Priority > list[right].Priority)
+				smallest = right;
+			if (smallest != index)
+			{
+				var temp = list[index];
+				list[index] = list[smallest];
+				list[smallest] = temp;
+				list.MinHeapify(last, smallest);
+			}
+		}
 	}
 
 	public interface IHasPriority
