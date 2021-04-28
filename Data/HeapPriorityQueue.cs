edef329b58fd11113d767391648ba36310f2a253
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2021-04-28 01:46:54 -0400

    Heap Priority Queue can use max- or min- heap

--- Data/HeapPriorityQueue.cs
+++ Data/HeapPriorityQueue.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 
 namespace Mox.Data
 {
@@ -6,8 +7,19 @@ namespace Mox.Data
 	{
 		readonly List<HeapNode> nodes = new List<HeapNode>();
 
+		public HeapPriorityQueue(bool withMaxHeap = true)
+		{
+			UsesMaxHeap = withMaxHeap;
+		}
+
+		[ShowInInspector] public bool UsesMaxHeap { get; private set; }
+
+		[ShowInInspector] private IEnumerable<HeapNode> Nodes => nodes;
+
+		[ShowInInspector]
 		public int Count => nodes.Count;
 
+		[Button]
 		public bool Dequeue(out T result)
 		{
 			if (nodes.Count <= 0)
@@ -18,14 +30,15 @@ namespace Mox.Data
 			result = nodes[0].Value;
 			nodes[0] = nodes[Count - 1];
 			nodes.RemoveAt(Count - 1);
-			nodes.Heapify(Count, 0);
+			nodes.Heapify(Count, 0, UsesMaxHeap);
 			return true;
 		}
 
+		[Button]
 		public void Enqueue(T item, int priority)
 		{
 			nodes.Insert(0, new HeapNode(item, priority));
-			nodes.Heapify(Count, 0);
+			nodes.Heapify(Count, 0, UsesMaxHeap);
 		}
 
 		class HeapNode : IHasPriority
