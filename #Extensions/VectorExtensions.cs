c9f073e633494543efa7f0015886e44f5961a9f0
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2021-07-11 18:29:39 -0400

    Random Vector2Int from List

--- #Extensions/VectorExtensions.cs
+++ #Extensions/VectorExtensions.cs
@@ -3,6 +3,7 @@
  * 
  */
 
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -15,6 +16,7 @@ namespace Mox
 
 		public static Vector3 ExpandAddZ(this Vector2 vec, float z) => new Vector3(vec.x, vec.y, z);
 		public static Vector3 ExpandYtoZ(this Vector2 vec) => new Vector3(vec.x, 0, vec.y);
+		public static Vector3 ExpandYtoZ(this Vector2Int vec) => new Vector3(vec.x, 0, vec.y);
 		public static Vector2 CollapseZtoY(this Vector3 vec) => new Vector2(vec.x, vec.z);
 
 		public static Vector3 RemoveY(this Vector3 vec) => new Vector3(vec.x, 0, vec.z);
@@ -33,6 +35,7 @@ namespace Mox
 
 		public static Vector3 Offset(this Vector3 position, Vector3 direction, float distance = 1f) => position + distance * direction.normalized;
 
+		public static Vector2Int PickRandom(this List<Vector2Int> list) => list[Random.Range(0, list.Count)];
 		public static Vector3 Widen(this Vector3 v, float amount) => v + Vector3.right * amount;
 
 		public static Vector3 Remap(this Vector3 v, Transform transform) => transform.forward * v.z + transform.up * v.y + transform.right * v.x;
